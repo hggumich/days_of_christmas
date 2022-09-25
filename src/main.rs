2b32ed9aba83689d3a79768ed151b3897ca9bcc8
Hector Garza <hggumich@gmail.com>
2022-09-25 14:47:49 -0400

    finish carol program

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
 fn main() {
-    println!("Hello, world!");
+    println!(
+        "This CLI apps prints the lyrics to the Christmas carol The Twelve Days of Christmas\n"
+    );
+
+    let days = [
+        "first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eighth", "ninth",
+        "tenth", "eleventh", "twelfth",
+    ];
+
+    let gifts = [
+        "A partridge in a pear tree",
+        "Two turtle doves",
+        "Three French hens",
+        "Four calling birds",
+        "Five gold rings",
+        "Six geese a-laying",
+        "Seven swans a-swimming",
+        "Eight maids a-milking",
+        "Nine ladies dancing",
+        "Ten lords a-leaping",
+        "Eleven pipers piping",
+        "Twelve drummers drumming",
+    ];
+
+    for i in 0..12 {
+        println!("On the {} day of Christmas,", days[i]);
+        println!("My true love gave to me: ");
+
+        for j in (0..i + 1).rev() {
+            if j == 0 && i != 0 {
+                println!("And {}", gifts[j]);
+            } else {
+                println!("{}", gifts[j]);
+            }
+        }
+        println!()
+    }
 }
